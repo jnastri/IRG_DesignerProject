affdd0c6978951729cbe83ad2155812e333ac9e2
jnastri <38079424+jnastri@users.noreply.github.com>
2018-04-05 00:04:25 -0400

    Finished More Scripts
    Finished up some more of our scripts marked that need to be done.

---
+++ Assets/Scripts/LerpObjectScript.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpObjectScript : MonoBehaviour
+{
+    public enum ObjectType { ThreeOrMore, TwoPointLerp}
+    public ObjectType typeOfMovingObject;
+    public float speed;
+    private float startSpeed;
+
+    [Header("Only if ThreeOrMore is Chosen")]
+    public Transform[] platformWaypoints;
+
+    [Header("Only if TwoPointLerp is Chosen")]
+    public Vector3 offSet;
+    public float timeDelay;
+    private float timeDelayStart;
+    private bool isDirectionFlipped;
+
+    private int currentWaypoint = 0;
+	// Use this for initialization
+	void Start ()
+    {
+        timeDelayStart = timeDelay;
+        startSpeed = speed;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(typeOfMovingObject == ObjectType.ThreeOrMore)
+        {
+            MultiplePointLerp();
+        }
+        else if(typeOfMovingObject == ObjectType.TwoPointLerp)
+        {
+            NormalLerp();
+        }
+	}
+
+    void NormalLerp()
+    {
+        timeDelay -= Time.deltaTime;
+        print(timeDelay);
+        if(timeDelay < 1)
+        {
+            speed -= Time.deltaTime;
+            if (timeDelay < 0)
+            {
+                isDirectionFlipped = !isDirectionFlipped;
+                timeDelay = timeDelayStart;
+                speed = startSpeed;
+            }
+        }
+
+        if (isDirectionFlipped)
+        {
+            transform.position = Vector3.Lerp(transform.position, transform.position - offSet, Time.deltaTime * speed * .67f);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, transform.position + offSet, Time.deltaTime * speed * .67f);
+        }
+    }
+
+    void MultiplePointLerp()
+    {
+        if (transform.position != platformWaypoints[currentWaypoint].transform.position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, platformWaypoints[currentWaypoint].transform.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            currentWaypoint++;
+        }
+
+        if (currentWaypoint >= platformWaypoints.Length)
+        {
+            currentWaypoint = 0;
+        }
+    }
+}
--- Assets/Scripts/ResourcePickupsScript.cs
+++ Assets/Scripts/ResourcePickupsScript.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResourcePickupsScript : MonoBehaviour {
-
+public class ResourcePickupsScript : MonoBehaviour
+{
+    public enum ResourceType { BatteryPickup, HealthPickup}
+    public ResourceType typeOfResource;
+    public int amountOfResource;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    void GiveResource()
+    {
+        if(typeOfResource == ResourceType.BatteryPickup)
+        {
+            //Give amount of resource once HUD is complete to the Battery
+        }
+        else if(typeOfResource == ResourceType.HealthPickup)
+        {
+            //Give amount of resource once HUD is complete to the Health
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            GiveResource();
+            Destroy(gameObject);
+        }
+    }
 }
