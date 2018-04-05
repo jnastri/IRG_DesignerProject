f6f93a7b0de2294cab41f3b060db5f8ef55bed3e
jnastri <38079424+jnastri@users.noreply.github.com>
2018-04-05 11:48:30 -0400

    Adding placeholders for folders
    Added Temps into each folder with nothing in it.

---
+++ Assets/Scripts/OpenDoorScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorScript : MonoBehaviour
+{
+    public GameObject openingDoor;
+
+    [HideInInspector]
+    public bool isOpen;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
