a81db6a4a3f847d628dd3e7657881f1b8425230d
jnastri <38079424+jnastri@users.noreply.github.com>
2018-04-05 17:24:07 -0400

    Laser Script
    This is a script for if players run into a laser. It will have a very slight delya then deal damage.

---
+++ Assets/Scripts/LaserDamageScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserDamageScript : MonoBehaviour
+{
+    public float damage;
+    public float damageInterval;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    void ProduceDamage()
+    {
+        //Inflict damage to player by sending damage to the HUD.
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            InvokeRepeating("ProduceDamage", .3f, damageInterval);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            CancelInvoke("ProduceDamage");
+        }
+    }
+}
