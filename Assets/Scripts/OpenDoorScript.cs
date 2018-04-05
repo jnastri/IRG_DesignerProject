84065d770cf4b1ceb4f2f9446fb2707a4549206e
jnastri <38079424+jnastri@users.noreply.github.com>
2018-04-05 15:22:05 -0400

    Interactable & OpenDoor Script
    This is the submission with the interactable and open door script. I made the interactable script based off the player to try and reduce the amount of scripts we need to go through for debugging. The open door script is done, but we need to discuss how we want them to open.

--- Assets/Scripts/OpenDoorScript.cs
+++ Assets/Scripts/OpenDoorScript.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class OpenDoorScript : MonoBehaviour
 {
     public GameObject openingDoor;
+    public Light[] doorLights;
 
-    [HideInInspector]
     public bool isOpen;
 	// Use this for initialization
 	void Start ()
     {
-		
+        ChangeLights();
 	}
 	
 	// Update is called once per frame
@@ -19,4 +19,50 @@ public class OpenDoorScript : MonoBehaviour
     {
 		
 	}
+    public void ChangeLights()
+    {
+        if (isOpen)
+        {
+            for(int i = 0; i < doorLights.Length; i++)
+            {
+                doorLights[i].color = Color.green;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < doorLights.Length; i++)
+            {
+                doorLights[i].color = Color.red;
+            }
+        }
+    }
+
+    void OpenTheDoor()
+    {
+        openingDoor.GetComponent<Animator>().SetBool("isActive", true);
+    }
+
+    void CloseDoor()
+    {
+        openingDoor.GetComponent<Animator>().SetBool("isActive", false);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            if (isOpen)
+            {
+                OpenTheDoor();
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            CloseDoor();
+        }
+    }
 }
