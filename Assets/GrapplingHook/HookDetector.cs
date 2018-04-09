ae6e752b503fba1652150760f0fdef970f11a23c
JRamirez <38079767+Voltate@users.noreply.github.com>
2018-04-09 11:03:53 -0400

    Adding unfinished grappling hook
    adding widow maker style grappling hook foundation (does not fully work)

---
+++ Assets/GrapplingHook/HookDetector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookDetector : MonoBehaviour {
+
+    //23:00 minute up to
+
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Hookable")
+        {
+            player.GetComponent<GrapplingHook>().hooked = true;
+            player.GetComponent<GrapplingHook>().hookedObj = other.gameObject;
+        }
+    }
+}
